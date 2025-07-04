0e2c50aa68cf96576f64ddb8f77046ba0f53a09b
ItxUzair1 <muhammaduzair1411@gmail.com>
2025-07-04 11:14:22 +0500

    Created Data Ingestion.py

--- src/logger.py
+++ src/logger.py
@@ -1,3 +1,5 @@
+# src/logger.py
+
 import logging
 from datetime import datetime
 import os
@@ -6,12 +8,14 @@ LOG_DIR = os.path.join(os.getcwd(), "logs")
 os.makedirs(LOG_DIR, exist_ok=True)
 
 LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-
 LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
 
-
+# This approach will always work, even if logging was already configured
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format='%(asctime)s - %(lineno)d %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO,
+    format='%(asctime)s - %(lineno)d %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(LOG_FILE_PATH),
+        logging.StreamHandler()
+    ]
 )
